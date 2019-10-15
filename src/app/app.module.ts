2e5fd8d7aa7ac9be78bfd1c4f5df59b5ed5e0576
Daniel Rodriguez <gameover_lifetime@outlook.com>
2019-10-14 22:36:40 -0500

    Small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ServicesService } from './services/services.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NavbarComponent } from './tools/navbar/navbar.component';
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AuthGuard } from './guard/auth.guard';
-import { TokenError } from '@angular/compiler/src/ml_parser/lexer';
-import { TokenInterceptorService } from './services/token-interceptor.service';
-import { ListaIncidentesComponent } from './components/lista-incidentes/lista-incidentes.component';
-import { IncidenteComponent } from './components/incidente/incidente.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module'
+import { ReactiveFormsModule } from '@angular/forms'
+import { AppComponent } from './app.component'
+import { ServicesService } from './services/services.service'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { InicioComponent } from './components/inicio/inicio.component'
+import { NavbarComponent } from './tools/navbar/navbar.component'
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { AuthGuard } from './guard/auth.guard'
+import { TokenError } from '@angular/compiler/src/ml_parser/lexer'
+import { TokenInterceptorService } from './services/token-interceptor.service'
+import { ListaIncidentesComponent } from './components/lista-incidentes/lista-incidentes.component'
+import { IncidenteComponent } from './components/incidente/incidente.component'
 
 @NgModule({
   declarations: [
@@ -37,12 +37,17 @@ import { IncidenteComponent } from './components/incidente/incidente.component';
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
-  providers: [ServicesService, AuthGuard, 
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptorService,
-    multi: true
-  }],
-  bootstrap: [AppComponent]
+  providers: [
+    ServicesService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,101 +1,100 @@
-import { ServicesService } from './../../services/services.service';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { ServicesService } from './../../services/services.service'
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl } from '@angular/forms'
 import { Router } from '@angular/router'
-import * as jwt_decode from 'jwt-decode';
+import * as jwt_decode from 'jwt-decode'
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: [
+    './inicio.component.css'
+  ]
 })
 export class InicioComponent implements OnInit {
+  incidentForm: FormGroup
+  leccionForm: FormGroup
+  incidents: Array<any> = []
+  logedUser: any = {}
+  adminUser: boolean
+  responsibleUser: boolean
+  selectedFile = null
 
-  incidentForm: FormGroup;
-  leccionForm: FormGroup;
-  incidents: Array<any> = [];
-  logedUser: any = {};
-  adminUser: boolean;
-  responsibleUser: boolean;
-  selectedFile = null;
+  constructor (private services: ServicesService, private router: Router) {}
 
-  constructor(
-    private services: ServicesService,
-    private router: Router,
-  ) { }
-
-
-  ngOnInit() {
+  ngOnInit () {
     //Inicializar forms
     this.incidentForm = new FormGroup({
       content: new FormControl(),
       category: new FormControl(),
       file: new FormControl(),
       tittle: new FormControl()
-    });
+    })
 
     this.leccionForm = new FormGroup({
       description: new FormControl(),
-      file: new FormControl(),
-    });
+      file: new FormControl()
+    })
 
     //Validar el tipo de usuario
-    var decoded_token = jwt_decode(this.services.token);
+    var decoded_token = jwt_decode(this.services.token)
     if (decoded_token.tipo == 'admin' || decoded_token.tipo == 'responsable') {
       if (decoded_token.tipo == 'admin') {
-        this.adminUser = true;
+        this.adminUser = true
       } else if (decoded_token.tipo == 'responsable') {
-        this.responsibleUser = true;
+        this.responsibleUser = true
       }
-    };
+    }
 
     //Obtener correo y nombre del usuario logeado
-    this.logedUser = JSON.parse(localStorage.getItem('user'));
+    this.logedUser = JSON.parse(localStorage.getItem('user'))
 
-    this.services.getIncidents().subscribe(res => {
-      this.incidents = res.incident;
-    });
+    this.services.getIncidents().subscribe((res) => {
+      this.incidents = res.incident
+    })
   }
 
-  onSelected(event) {
+  /*
+  onIncidentSelected (event) {
     if (event.target.files.length > 0) {
-      this.selectedFile = event.target.files[0];
-      this.incidentForm.get('file').setValue(this.selectedFile);
+      this.selectedFile = event.target.files[0]
+      this.incidentForm.get('file').setValue(this.selectedFile)
     }
   }
 
-  onSelected1(event) {
+  onLessonFileSelected (event) {
     if (event.target.files.length > 0) {
-      this.selectedFile = event.target.files[0];
-      this.leccionForm.get('file').setValue(this.selectedFile);
+      this.selectedFile = event.target.files[0]
+      this.leccionForm.get('file').setValue(this.selectedFile)
     }
   }
+  */
 
-  enviarReporte(): void {
-    this.services.saveReport(this.incidentForm.value).subscribe(res => {
-      console.log(res);
-    });
+  enviarReporte (): void {
+    this.services.saveReport(this.incidentForm.value).subscribe((res) => {
+      console.log(res)
+    })
   }
 
-  enviarLeccion(): void {
-    console.log(this.leccionForm.value);
-    
-    this.services.saveLesson(this.leccionForm.value).subscribe(res => {
-      console.log(res);
-    });
+  enviarLeccion (): void {
+    this.services.saveLesson(this.leccionForm.value).subscribe((res) => {
+      console.log(res)
+    })
   }
 
-  editProfile() {
-    this.router.navigateByUrl("/profile/" + this.logedUser.email)
+  editProfile () {
+    this.router.navigateByUrl('/profile/' + this.logedUser.email)
   }
 
-  registrarResponsable() {
-    this.router.navigate(['/register']);
+  registrarResponsable () {
+    this.router.navigate([
+      '/register'
+    ])
   }
 
-  listaIncidentes() {
-    this.router.navigate(['/incidentes']);
+  listaIncidentes () {
+    this.router.navigate([
+      '/incidentes'
+    ])
   }
-
-
 }
